cc257d7ff84dd666728f7db559712b97143a6b20
xxxkhalid <45016223+xxxkhalid@users.noreply.github.com>
2018-11-20 19:40:57 +0000

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "c";
-var adminprefix = 'c'
+var adminprefix = 'Y'
 
-const developers = ["472413769700474901","id"]
+const developers = ["344576482154250241"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
